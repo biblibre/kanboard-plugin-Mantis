ae1bf0788f86ac65bd092b83334c4cac0188d404
Julian Maurice <julian.maurice@biblibre.com>
2018-08-10 16:00:34 +0200

    Set autofocus on mantis id form field

--- Template/task/import.php
+++ Template/task/import.php
@@ -3,4 +3,4 @@
 </div>
 
 <?= $this->form->label(t('Issue ID'), 'id') ?>
-<?= $this->form->text('id', $values, array(), array('required')) ?>
+<?= $this->form->text('id', $values, array(), array('required', 'autofocus')) ?>
